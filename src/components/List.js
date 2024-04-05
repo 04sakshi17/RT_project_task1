2bff9275c36d6b645101850623be276c7ceaa763
sakshi <sakshimaurya017@gmail.com>
2024-04-05 20:40:49 +0530

    add files

--- src/components/List.js
+++ src/components/List.js
@@ -1,40 +1,47 @@
-import React, { useState, useEffect } from "react";
-import Character from "./Character";
+import Character from './Character';
+ //import { useState } from 'react';
+ import { useEffect, useState } from 'react';
 function List() {
-  const [characters, setCharacters] = useState([]);
-  const [loading, setLoading] = useState(true);
+// return <h2>Characters</h2>;
+const [loading, setLoading] = useState(true);
+const [characters, setCharacters] = useState([]);
+useEffect(() => {
+async function fetchData() {
+const data = await fetch(
+'https://rickandmortyapi.com/api/character');
+const { results } = await data.json();
+setCharacters(results);
+setLoading(false);
+}
+fetchData();
+}, [characters.length]);
+// return (
+//  <div>
+//  <h2>Characters</h2>
+//  <Character />
+//  <Character />
+//  </div>
+//  );
+// }
 
-  useEffect(() => {
-    async function fetchData() {
-      const data = await fetch("https://rickandmortyapi.com/api/character");
-      const { results } = await data.json();
-      setCharacters(results);
-      setLoading(false);
-    }
-    fetchData();
-  }, [characters.length]);
-  return (
-    <>
-      <div>
-        <h2>Characters</h2>
-        <div className='row'>
-        {loading ? (
-          <div>Loading.........</div>
-        ) : (
-          <div>
-            {characters.map((character) => (
-              <Character
-                key={characters.id}
-                name={character.name}
-                origin={character.origin}
-                image={character.image}
-              />
-            ))}
-          </div>
-        )}
-        </div>
-      </div>
-    </>
-  );
+return (
+<div>
+<h2>Characters</h2>
+<div className='row'>
+ {loading ? (
+ <div>Loading...</div>
+ ) : (
+ characters.map((character) => (
+ <Character
+ key={character.id}
+ name={character.name}
+ origin={character.origin}
+ image={character.image}
+ />
+ ))
+ )}
+</div>
+</div>
+);
 }
 export default List;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
-// console.log('rick and Morty');
-import {createRoot} from "react-dom/client";
-import List from "./components/List";
-import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import ReactDOM from 'react-dom/client';
+// const container = document.getElementById('app');
+// const root = ReactDOM.createRoot(container);
+// root.render(<App />);
+
+// function App() {
+//  return <h1>Rick and Morty</h1>;
+// }
+
+
+import { createRoot } from 'react-dom/client';
+ import List from './components/List';
+ import 'bootstrap/dist/css/bootstrap.min.css';
+ const container = document.getElementById('root');
+ const root = createRoot(container);
+  root.render(<App />);
 function App() {
-  return (
-    <>
-      <div className="container">
-        {/* <h1>Rick and Morty</h1> */}
-        <nav className="navbar sticky-top navbar-light bg-dark">
-          <h1 className="navbar-brand text-light">Rick and Morty</h1>
-        </nav>
-        <List />
-      </div>
-    </>
-  );
+//return <h1>Rick and Morty</h1>;
+ return (
+ <div>
+   <div className='container'></div>
+   <nav className='navbar sticky-top navbar-light
+ bg-dark'>
+<h1 className='navbar-brand text-light'>
+ Rick and Morty</h1>
+</nav>
+ <h1 className='text-primary'>Rick and Morty</h1>
+ <List />
+ </div>
+ );
 }
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+
+
