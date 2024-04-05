001d7f4f98ce779f264f9d47c3a2fb27103386b0
sakshi <sakshimaurya017@gmail.com>
2024-04-05 14:30:23 +0530

    add files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('rick and Morty');
+// console.log('rick and Morty');
+import ReactDOM from 'react-dom/client';
+function App() {
+ return <h1>Rick and Morty</h1>;
+}
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
